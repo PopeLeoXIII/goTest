d73423dacff1b79df9ff8a115a5f8ad5afe6a832
dkuryshova <superegd99@gmail.com>
2022-04-06 14:10:39 +0400

    improved formatting

--- getRand.go
+++ getRand.go
@@ -10,55 +10,77 @@ import (
 	"time"
 )
 
-func gen(c chan int, maxValue int) {
-	id := rand.Intn(maxValue)
-	for i := 0; ; i++ {
-		el := 1 + rand.Intn(maxValue)
-		fmt.Println(el, id)
-		c <- el
+// gen генерирует случайные числа
+func gen(c chan<- int, maxValue int) {
+	//id := rand.Intn(maxValue)
+	for {
+		randomNumber := 1 + rand.Intn(maxValue)
+		//fmt.Println(el, id)
+		c <- randomNumber
 	}
 }
 
-func counter(c chan int, n int) []int {
+// counter следит за уникальность сгенерированных чисел
+func counter(c <-chan int, n int) []int {
 	counter := make(map[int]int)
-	arr := make([]int, 0, n)
+	result := make([]int, 0, n)
 
 	for i := 0; i < n; {
-		el := <-c
-		if counter[el] == 0 {
-			counter[el] = 1
-			arr = append(arr, el)
-			fmt.Println(el)
+		randomNumber := <-c
+		if counter[randomNumber] == 0 {
+			counter[randomNumber] = 1
+			result = append(result, randomNumber)
+			//fmt.Println(el)
 			i++
 		}
 	}
-	return arr
+	return result
+}
+
+type appContext struct {
+	n, maxValue, genN, cb int
+}
+
+func parseFlags() appContext {
+	//var n, maxValue, genN, cb int
+	context := appContext{}
+
+	flag.IntVar(&context.n, "n", 10, "number of random numbers")
+	flag.IntVar(&context.cb, "cb", 1, "chenal buf")
+	flag.IntVar(&context.genN, "gen", 1, "number of generators random numbers")
+	flag.IntVar(&context.maxValue, "max", 10, "maximum value of random numbers")
+	flag.Parse()
+
+	if context.n <= 0 || context.maxValue <= 0 ||
+		context.cb <= 0 || context.genN <= 0 {
+		fmt.Println("параметры должны быть > 0")
+		os.Exit(1)
+	}
+
+	if context.n > context.maxValue {
+		fmt.Println("неврные параметры ввода")
+		os.Exit(1)
+	}
+	return context
 }
 
 func main() {
-	var n, maxValue, genN int
-	flag.IntVar(&n, "n", 10, "number of random numbers")
-	flag.IntVar(&genN, "gen", 2, "number of generators random numbers")
-	flag.IntVar(&maxValue, "max", 10, "maximum value of random numbers")
-	flag.Parse()
 
-	if n <= 0 || maxValue <= 0 {
-		fmt.Println("параметры должны быть > 0")
-		return
-	}
-
-	if n > maxValue {
-		fmt.Println("неврные параметры ввода")
-		return
-	}
+	context := parseFlags()
 
 	rand.Seed(time.Now().UnixNano())
-	var c chan int = make(chan int)
-	for i := 0; i < genN; i++ {
-		go gen(c, maxValue)
+	var c chan int = make(chan int, context.cb)
+
+	t := time.Now().UnixNano()
+
+	for i := 0; i < context.genN; i++ {
+		go gen(c, context.maxValue)
 	}
-	arr := counter(c, n)
+
+	arr := counter(c, context.n)
 	sort.Ints(arr)
+	t = time.Now().UnixNano() - t
+	fmt.Println("вермя выполнения ", t/100000)
 	fmt.Println(arr)
 }
 
